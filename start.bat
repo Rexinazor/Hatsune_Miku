ffbfe82d3dbc3c4bd34ce8f1f5de667ed61549e3
Ｒｅｘｉｎａｚｏｒ <85327329+Rexinazor@users.noreply.github.com>
2021-07-24 19:54:50 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE Layla Robot
+TITLE Hatsune_Miku
 :: Enables virtual env mode and then starts Rika
-env\scripts\activate.bat && py -m LaylaRobot
+env\scripts\activate.bat && py -m Hatsune_Miku
